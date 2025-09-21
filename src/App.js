240d7429c9cd615637e072e65c843d118bcba73c
atsiarnouskaya <atsiarnouskaya@mat.umk.pl>
2025-09-21 19:15:49 +0300

    Backup 2

--- src/App.js
+++ src/App.js
@@ -3,40 +3,20 @@ import RecipeService from "./API/RecipeService";
 import { useEffect, useState } from "react";
 import useFetching from "./hooks/useFetching";
 import RecipeFormComponent from "./Components/Form/RecipeFormComponent";
-import AllRecipesComponent from "./Components/AllRecipes/AllRecipesComponent";
+import AllRecipes from "./Pages/AllRecipes";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 
 function App() {
-    const [recipes, setRecipes] = useState([]);
-
-    const [fetchRecipes, isLoading, error] = useFetching(async () => {
-        const response = await RecipeService.getRecipes()
-        console.log(response.data)
-        setRecipes(response.data)
-    })
-
-    const [saveRecipe, isSaving, recipeSavingError] = useFetching(async (recipe) => {
-        const response = await RecipeService.saveRecipe(recipe)
-        console.log(response.data)
-        setRecipes([...recipes, response.data])
-        fetchRecipes()
-    })
-
-    const savingRecipe = (recipe) => {
-        saveRecipe(recipe)
-    }
-
-
-    useEffect(() => {
-        fetchRecipes();
-    }, []);
-
-    if (isLoading) return <h2>Загрузка...</h2>;
-
     return (
         <div className="App">
+            <BrowserRouter>
+                <Routes>
+                    <Route element={<AllRecipes/>}
+                           path={"/allRecipes"}/>
 
-            <AllRecipesComponent recipes={recipes} />
-            <RecipeFormComponent saveRecipe={savingRecipe} />
+                </Routes>
+            </BrowserRouter>
 
         </div>
     );
